bf68b673d4c7ce0e836d50486da250f2596e8952
Scott Hammack <scott@hammack.exposed>
2016-05-20 14:41:12 -0700

    fuck git

--- ephemeratron2000.py
+++ ephemeratron2000.py
@@ -4,7 +4,7 @@
 import re, requests, sys, tweepy
 from tweepy.streaming import StreamListener
 
-from twitter_secrets import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
+from twitter_secrets import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET, SCREEN_NAME
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
@@ -12,17 +12,14 @@ api = tweepy.API(auth)
 
 class Listener(StreamListener):
 	def on_status(self, status):
-		delete_old_tweet()
-		return True
+		return delete_old_tweet()
 	
 	def on_error(self, status):
 		print status
 
 def delete_old_tweet():
-	#tweets = api.user_timeline(screen_name = 'czircon',count=200)
-#	for status in tweepy.Cursor(api.user_timeline(screen_name = 'czircon', count=200)).items():
-	for status in tweepy.Cursor(api.user_timeline, screen_name = 'czircon', count=200, include_rts=True).items():
-		if not "Verifying myself:" in status.text and not status.favorited:
+	for status in tweepy.Cursor(api.user_timeline, screen_name = SCREEN_NAME, count = 200, include_rts = True).items():
+		if not "Verifying myself:" in status.text and not (status.favorited and not status.retweeted):
 			oldest_tweet = status
 	print "oldest: " + oldest_tweet.text
 
@@ -31,14 +28,13 @@ def delete_old_tweet():
 	if len(sys.argv) > 1 and sys.argv[1] == "debug":
 		if raw_input("Destroy tweet? ").lower() != 'y':
 			confirmation = False
+			return False
 
 	if confirmation:
 		print "Destroying."
 		api.destroy_status(oldest_tweet.id)
-# post the tweet
-u = api.get_user(screen_name = 'czircon' )
-stream = tweepy.Stream(auth, Listener())
-stream.filter(follow=[str(u.id)])
-#tweets = api.user_timeline(screen_name = 'czircon',count=200)
+		return True
 
-#delete_old_tweet()
+u = api.get_user(screen_name = SCREEN_NAME)
+stream = tweepy.Stream(auth, Listener())
+stream.filter(follow = [str(u.id)])

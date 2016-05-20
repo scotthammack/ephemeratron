1f342db865b14a6d3b9f580b424903e813d8fb0b
Scott Hammack <scott@hammack.exposed>
2016-05-20 16:27:47 -0700

    fixed some stuff

--- ephemeratron2000.py
+++ ephemeratron2000.py
@@ -6,12 +6,16 @@ from tweepy.streaming import StreamListener
 
 from twitter_secrets import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET, SCREEN_NAME
 
+DEBUG = ( len(sys.argv) > 1 and sys.argv[1] == "debug" )
+
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 class Listener(StreamListener):
 	def on_status(self, status):
+		if status.user.screen_name != SCREEN_NAME or api.get_user(screen_name = SCREEN_NAME).statuses_count <= 69:
+			return False
 		delete_old_tweet()
 		return True
 	
@@ -19,25 +23,24 @@ class Listener(StreamListener):
 		print status
 
 def delete_old_tweet():
-	if api.get_user(screen_name = SCREEN_NAME).statuses_count <= 69:
-		return False
+	oldest_tweet = None
 
 	for status in tweepy.Cursor(api.user_timeline, screen_name = SCREEN_NAME, count = 200, include_rts = True).items():
-		if not "Verifying myself:" in status.text and not (status.favorited and not status.retweeted):
+		if not (status.favorited and not status.retweeted) and not status.text.startswith("Verifying myself:"):
 			oldest_tweet = status
-	print "oldest: " + oldest_tweet.text
 
-	confirmation = True
+	if not oldest_tweet:
+		print "Couldn't find an oldest tweet. :("
+		return False
 
-	if len(sys.argv) > 1 and sys.argv[1] == "debug":
-		if raw_input("Destroy tweet? ").lower() != 'y':
-			confirmation = False
-			return False
+	print "Oldest tweet: " + oldest_tweet.text
 
-	if confirmation:
-		print "Destroying."
-		api.destroy_status(oldest_tweet.id)
-		return True
+	if DEBUG and not raw_input("Destroy tweet? ").lower().startswith('y'):
+		return False
+
+	print "Destroying."
+	api.destroy_status(oldest_tweet.id)
+	return True
 
 u = api.get_user(screen_name = SCREEN_NAME)
 stream = tweepy.Stream(auth, Listener())
